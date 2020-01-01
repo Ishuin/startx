3c0d254ae919731ac8778e131893608f7531ba52
Ishuin <ishu.kumars@gmail.com>
2020-01-01 19:37:38 +0530

    Added Django Admin Tables

--- tweet/Post/admin.py
+++ tweet/Post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Post, PostLike
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
